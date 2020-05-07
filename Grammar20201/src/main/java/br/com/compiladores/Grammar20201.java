2ea9f5f56e55bedd155606e2711cf2b93e53ad96
mauriciopasetto <mauriciopasetto@gmail.com>
2020-05-07 12:50:03 -0300

    atualizacao sintatico

--- Grammar20201/src/main/java/br/com/compiladores/Grammar20201.java
+++ Grammar20201/src/main/java/br/com/compiladores/Grammar20201.java
@@ -130,11 +130,11 @@ public class Grammar20201 implements Grammar20201Constants {
     switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
     case TRUE:
       LinhaTrue();
-      AlternanciaResultadol();
+      LinhaUntruel();
       break;
     case UNTRUE:
       LinhaUntrue();
-      AlternanciaResultadol();
+      LinhaTruel();
       break;
     default:
       jj_la1[2] = jj_gen;
@@ -143,18 +143,25 @@ public class Grammar20201 implements Grammar20201Constants {
     }
   }
 
-  static final public void AlternanciaResultadol() throws ParseException {
+  static final public void LinhaUntruel() throws ParseException {
     switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
-    case TRUE:
-      LinhaTrue();
-      break;
     case UNTRUE:
       LinhaUntrue();
       break;
     default:
       jj_la1[3] = jj_gen;
-      jj_consume_token(-1);
-      throw new ParseException();
+
+    }
+  }
+
+  static final public void LinhaTruel() throws ParseException {
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case TRUE:
+      LinhaTrue();
+      break;
+    default:
+      jj_la1[4] = jj_gen;
+
     }
   }
 
@@ -177,11 +184,10 @@ public class Grammar20201 implements Grammar20201Constants {
 // dec comando de saida de dados
   static final public void ComandoSaidaDados() throws ParseException {
     jj_consume_token(WRITE);
-    LinhasWrhiters();
-
+    LinhasWriters();
   }
 
-  static final public void LinhasWrhiters() throws ParseException {
+  static final public void LinhasWriters() throws ParseException {
     switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
     case ALL:
       LinhaWriteAll();
@@ -190,7 +196,7 @@ public class Grammar20201 implements Grammar20201Constants {
       LinhaWrite();
       break;
     default:
-      jj_la1[4] = jj_gen;
+      jj_la1[5] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -200,7 +206,7 @@ public class Grammar20201 implements Grammar20201Constants {
     jj_consume_token(ALL);
     jj_consume_token(THIS);
     jj_consume_token(COLCH_ESQ);
-    ListaDeIdentificadores();
+    ListaDeIdentificadoresVet();
     jj_consume_token(COLCH_DIR);
     jj_consume_token(PONTO);
   }
@@ -208,7 +214,7 @@ public class Grammar20201 implements Grammar20201Constants {
   static final public void LinhaWrite() throws ParseException {
     jj_consume_token(THIS);
     jj_consume_token(COLCH_ESQ);
-    ListaDeIdentificadores();
+    ListaDeIdentificadoresVet();
     jj_consume_token(COLCH_DIR);
     jj_consume_token(PONTO);
   }
@@ -218,14 +224,15 @@ public class Grammar20201 implements Grammar20201Constants {
     jj_consume_token(READ);
     jj_consume_token(THIS);
     jj_consume_token(COLCH_ESQ);
-    ListaDeIdentificadores();
+    ListaDeIdentificadoresVet();
     jj_consume_token(COLCH_DIR);
   }
 
+// dec comando de atribuicao
   static final public void ComandoAtribuicao() throws ParseException {
     jj_consume_token(DESIGNATE);
     jj_consume_token(THIS);
-    ListaDeIdentificadores();
+    ListaDeIdentificadoresVet();
     jj_consume_token(AS);
     Expressao();
   }
@@ -242,25 +249,60 @@ public class Grammar20201 implements Grammar20201Constants {
   }
 
   static final public void DecConstEVar() throws ParseException {
-    if (jj_2_1(2)) {
-      DecAsConstant();
-      DecAsVariable();
-    } else if (jj_2_2(2)) {
-      DecAsVariable();
-      DecAsConstant();
-    } else if (jj_2_3(2)) {
-      DecAsVariable();
-    } else if (jj_2_4(2)) {
-      DecAsConstant();
-    } else {
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case AS:
+      jj_consume_token(AS);
+      DecConstEVarl();
+      break;
+    default:
+      jj_la1[6] = jj_gen;
 
     }
   }
 
+  static final public void DecConstEVarl() throws ParseException {
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case CONSTANT:
+      jj_consume_token(CONSTANT);
+      ListaDecAsConstant();
+      DecAsVariable();
+      break;
+    case VARIABLE:
+      jj_consume_token(VARIABLE);
+      ListaDecAsVariable();
+      DecAsConstant();
+      break;
+    default:
+      jj_la1[7] = jj_gen;
+      jj_consume_token(-1);
+      throw new ParseException();
+    }
+  }
+
   static final public void DecAsConstant() throws ParseException {
-    jj_consume_token(AS);
-    jj_consume_token(CONSTANT);
-    ListaDecAsConstant();
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case AS:
+      jj_consume_token(AS);
+      jj_consume_token(CONSTANT);
+      ListaDecAsConstant();
+      break;
+    default:
+      jj_la1[8] = jj_gen;
+
+    }
+  }
+
+  static final public void DecAsVariable() throws ParseException {
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case AS:
+      jj_consume_token(AS);
+      jj_consume_token(VARIABLE);
+      ListaDecAsVariable();
+      break;
+    default:
+      jj_la1[9] = jj_gen;
+
+    }
   }
 
   static final public void ListaDecAsConstant() throws ParseException {
@@ -285,7 +327,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ListaDecAsConstantl();
       break;
     default:
-      jj_la1[5] = jj_gen;
+      jj_la1[10] = jj_gen;
 
     }
   }
@@ -305,16 +347,10 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(LOGIC);
       break;
     default:
-      jj_la1[6] = jj_gen;
+      jj_la1[11] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
-  }
-
-  static final public void DecAsVariable() throws ParseException {
-    jj_consume_token(AS);
-    jj_consume_token(VARIABLE);
-    ListaDecAsVariable();
   }
 
   static final public void ListaDecAsVariable() throws ParseException {
@@ -335,7 +371,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ListaDecAsVariablel();
       break;
     default:
-      jj_la1[7] = jj_gen;
+      jj_la1[12] = jj_gen;
 
     }
   }
@@ -356,7 +392,7 @@ public class Grammar20201 implements Grammar20201Constants {
 
       break;
     default:
-      jj_la1[8] = jj_gen;
+      jj_la1[13] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -374,7 +410,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(CLITERAL);
       break;
     default:
-      jj_la1[9] = jj_gen;
+      jj_la1[14] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -388,7 +424,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(COLCH_DIR);
       break;
     default:
-      jj_la1[10] = jj_gen;
+      jj_la1[15] = jj_gen;
 
     }
   }
@@ -401,7 +437,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(CLITERAL);
       break;
     default:
-      jj_la1[11] = jj_gen;
+      jj_la1[16] = jj_gen;
 
     }
   }
@@ -417,7 +453,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(COLCH_DIR);
       break;
     default:
-      jj_la1[12] = jj_gen;
+      jj_la1[17] = jj_gen;
 
     }
   }
@@ -441,7 +477,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ListaDeIdentificadoresl();
       break;
     default:
-      jj_la1[13] = jj_gen;
+      jj_la1[18] = jj_gen;
 
     }
   }
@@ -460,7 +496,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ListaDeIdentificadoresl();
       break;
     default:
-      jj_la1[14] = jj_gen;
+      jj_la1[19] = jj_gen;
 
     }
   }
@@ -481,7 +517,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ListaDeIdentificadoresVetl();
       break;
     default:
-      jj_la1[15] = jj_gen;
+      jj_la1[20] = jj_gen;
 
     }
   }
@@ -494,7 +530,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(COLCH_DIR);
       break;
     default:
-      jj_la1[16] = jj_gen;
+      jj_la1[21] = jj_gen;
 
     }
   }
@@ -542,7 +578,7 @@ public class Grammar20201 implements Grammar20201Constants {
       ExpressaoAtitmeticaOuLogica();
       break;
     default:
-      jj_la1[17] = jj_gen;
+      jj_la1[22] = jj_gen;
 
     }
   }
@@ -571,7 +607,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(OU);
       break;
     default:
-      jj_la1[18] = jj_gen;
+      jj_la1[23] = jj_gen;
 
     }
   }
@@ -609,7 +645,7 @@ public class Grammar20201 implements Grammar20201Constants {
       MediaPrioridade();
       break;
     default:
-      jj_la1[19] = jj_gen;
+      jj_la1[24] = jj_gen;
 
     }
   }
@@ -627,7 +663,7 @@ public class Grammar20201 implements Grammar20201Constants {
       MaiorPrioridade();
       break;
     default:
-      jj_la1[20] = jj_gen;
+      jj_la1[25] = jj_gen;
 
     }
   }
@@ -665,7 +701,7 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(PAREN_DIR);
       break;
     default:
-      jj_la1[21] = jj_gen;
+      jj_la1[26] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -677,69 +713,9 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_consume_token(CINTEIRO);
       break;
     default:
-      jj_la1[22] = jj_gen;
+      jj_la1[27] = jj_gen;
 
     }
-  }
-
-  static private boolean jj_2_1(int xla) {
-    jj_la = xla; jj_lastpos = jj_scanpos = token;
-    try { return !jj_3_1(); }
-    catch(LookaheadSuccess ls) { return true; }
-    finally { jj_save(0, xla); }
-  }
-
-  static private boolean jj_2_2(int xla) {
-    jj_la = xla; jj_lastpos = jj_scanpos = token;
-    try { return !jj_3_2(); }
-    catch(LookaheadSuccess ls) { return true; }
-    finally { jj_save(1, xla); }
-  }
-
-  static private boolean jj_2_3(int xla) {
-    jj_la = xla; jj_lastpos = jj_scanpos = token;
-    try { return !jj_3_3(); }
-    catch(LookaheadSuccess ls) { return true; }
-    finally { jj_save(2, xla); }
-  }
-
-  static private boolean jj_2_4(int xla) {
-    jj_la = xla; jj_lastpos = jj_scanpos = token;
-    try { return !jj_3_4(); }
-    catch(LookaheadSuccess ls) { return true; }
-    finally { jj_save(3, xla); }
-  }
-
-  static private boolean jj_3R_1() {
-    if (jj_scan_token(AS)) return true;
-    if (jj_scan_token(CONSTANT)) return true;
-    return false;
-  }
-
-  static private boolean jj_3_3() {
-    if (jj_3R_2()) return true;
-    return false;
-  }
-
-  static private boolean jj_3_4() {
-    if (jj_3R_1()) return true;
-    return false;
-  }
-
-  static private boolean jj_3_2() {
-    if (jj_3R_2()) return true;
-    return false;
-  }
-
-  static private boolean jj_3R_2() {
-    if (jj_scan_token(AS)) return true;
-    if (jj_scan_token(VARIABLE)) return true;
-    return false;
-  }
-
-  static private boolean jj_3_1() {
-    if (jj_3R_1()) return true;
-    return false;
   }
 
   static private boolean jj_initialized_once = false;
@@ -751,10 +727,8 @@ public class Grammar20201 implements Grammar20201Constants {
   /** Next token. */
   static public Token jj_nt;
   static private int jj_ntk;
-  static private Token jj_scanpos, jj_lastpos;
-  static private int jj_la;
   static private int jj_gen;
-  static final private int[] jj_la1 = new int[23];
+  static final private int[] jj_la1 = new int[28];
   static private int[] jj_la1_0;
   static private int[] jj_la1_1;
   static private int[] jj_la1_2;
@@ -764,17 +738,14 @@ public class Grammar20201 implements Grammar20201Constants {
       jj_la1_init_2();
    }
    private static void jj_la1_init_0() {
-      jj_la1_0 = new int[] {0x37000000,0x37000000,0x80000000,0x80000000,0x8000400,0x0,0xf00000,0x0,0xf00000,0x0,0x0,0x1000,0x2000,0x0,0x0,0x0,0x0,0x80000000,0x0,0x0,0x0,0x80000000,0x0,};
+      jj_la1_0 = new int[] {0x37000000,0x37000000,0x80000000,0x0,0x80000000,0x8000400,0x80000,0x50000,0x80000,0x80000,0x0,0xf00000,0x0,0xf00000,0x0,0x0,0x1000,0x2000,0x0,0x0,0x0,0x0,0x80000000,0x0,0x0,0x0,0x80000000,0x0,};
    }
    private static void jj_la1_init_1() {
-      jj_la1_1 = new int[] {0x0,0x0,0x1,0x1,0x0,0x20000000,0x0,0x20000000,0x0,0xd000000,0x2,0x0,0x0,0x20000000,0x10,0x10,0x2,0x2d801fa1,0x40c000,0x3b0000,0x40000,0x2d800021,0x4000000,};
+      jj_la1_1 = new int[] {0x0,0x0,0x1,0x1,0x0,0x0,0x0,0x0,0x0,0x0,0x20000000,0x0,0x20000000,0x0,0xd000000,0x2,0x0,0x0,0x20000000,0x10,0x10,0x2,0x2d801fa1,0x40c000,0x3b0000,0x40000,0x2d800021,0x4000000,};
    }
    private static void jj_la1_init_2() {
-      jj_la1_2 = new int[] {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,};
+      jj_la1_2 = new int[] {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,};
    }
-  static final private JJCalls[] jj_2_rtns = new JJCalls[4];
-  static private boolean jj_rescan = false;
-  static private int jj_gc = 0;
 
   /** Constructor with InputStream. */
   public Grammar20201(java.io.InputStream stream) {
@@ -794,8 +765,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -809,8 +779,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   /** Constructor. */
@@ -827,8 +796,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -838,8 +806,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   /** Constructor with generated Token Manager. */
@@ -855,8 +822,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -865,8 +831,7 @@ public class Grammar20201 implements Grammar20201Constants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 23; i++) jj_la1[i] = -1;
-    for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
+    for (int i = 0; i < 28; i++) jj_la1[i] = -1;
   }
 
   static private Token jj_consume_token(int kind) throws ParseException {
@@ -876,44 +841,11 @@ public class Grammar20201 implements Grammar20201Constants {
     jj_ntk = -1;
     if (token.kind == kind) {
       jj_gen++;
-      if (++jj_gc > 100) {
-        jj_gc = 0;
-        for (int i = 0; i < jj_2_rtns.length; i++) {
-          JJCalls c = jj_2_rtns[i];
-          while (c != null) {
-            if (c.gen < jj_gen) c.first = null;
-            c = c.next;
-          }
-        }
-      }
       return token;
     }
     token = oldToken;
     jj_kind = kind;
     throw generateParseException();
-  }
-
-  static private final class LookaheadSuccess extends java.lang.Error { }
-  static final private LookaheadSuccess jj_ls = new LookaheadSuccess();
-  static private boolean jj_scan_token(int kind) {
-    if (jj_scanpos == jj_lastpos) {
-      jj_la--;
-      if (jj_scanpos.next == null) {
-        jj_lastpos = jj_scanpos = jj_scanpos.next = token_source.getNextToken();
-      } else {
-        jj_lastpos = jj_scanpos = jj_scanpos.next;
-      }
-    } else {
-      jj_scanpos = jj_scanpos.next;
-    }
-    if (jj_rescan) {
-      int i = 0; Token tok = token;
-      while (tok != null && tok != jj_scanpos) { i++; tok = tok.next; }
-      if (tok != null) jj_add_error_token(kind, i);
-    }
-    if (jj_scanpos.kind != kind) return true;
-    if (jj_la == 0 && jj_scanpos == jj_lastpos) throw jj_ls;
-    return false;
   }
 
 
@@ -946,33 +878,6 @@ public class Grammar20201 implements Grammar20201Constants {
   static private java.util.List<int[]> jj_expentries = new java.util.ArrayList<int[]>();
   static private int[] jj_expentry;
   static private int jj_kind = -1;
-  static private int[] jj_lasttokens = new int[100];
-  static private int jj_endpos;
-
-  static private void jj_add_error_token(int kind, int pos) {
-    if (pos >= 100) return;
-    if (pos == jj_endpos + 1) {
-      jj_lasttokens[jj_endpos++] = kind;
-    } else if (jj_endpos != 0) {
-      jj_expentry = new int[jj_endpos];
-      for (int i = 0; i < jj_endpos; i++) {
-        jj_expentry[i] = jj_lasttokens[i];
-      }
-      jj_entries_loop: for (java.util.Iterator<?> it = jj_expentries.iterator(); it.hasNext();) {
-        int[] oldentry = (int[])(it.next());
-        if (oldentry.length == jj_expentry.length) {
-          for (int i = 0; i < jj_expentry.length; i++) {
-            if (oldentry[i] != jj_expentry[i]) {
-              continue jj_entries_loop;
-            }
-          }
-          jj_expentries.add(jj_expentry);
-          break jj_entries_loop;
-        }
-      }
-      if (pos != 0) jj_lasttokens[(jj_endpos = pos) - 1] = kind;
-    }
-  }
 
   /** Generate ParseException. */
   static public ParseException generateParseException() {
@@ -982,7 +887,7 @@ public class Grammar20201 implements Grammar20201Constants {
       la1tokens[jj_kind] = true;
       jj_kind = -1;
     }
-    for (int i = 0; i < 23; i++) {
+    for (int i = 0; i < 28; i++) {
       if (jj_la1[i] == jj_gen) {
         for (int j = 0; j < 32; j++) {
           if ((jj_la1_0[i] & (1<<j)) != 0) {
@@ -1004,9 +909,6 @@ public class Grammar20201 implements Grammar20201Constants {
         jj_expentries.add(jj_expentry);
       }
     }
-    jj_endpos = 0;
-    jj_rescan_token();
-    jj_add_error_token(0, 0);
     int[][] exptokseq = new int[jj_expentries.size()][];
     for (int i = 0; i < jj_expentries.size(); i++) {
       exptokseq[i] = jj_expentries.get(i);
@@ -1020,44 +922,6 @@ public class Grammar20201 implements Grammar20201Constants {
 
   /** Disable tracing. */
   static final public void disable_tracing() {
-  }
-
-  static private void jj_rescan_token() {
-    jj_rescan = true;
-    for (int i = 0; i < 4; i++) {
-    try {
-      JJCalls p = jj_2_rtns[i];
-      do {
-        if (p.gen > jj_gen) {
-          jj_la = p.arg; jj_lastpos = jj_scanpos = p.first;
-          switch (i) {
-            case 0: jj_3_1(); break;
-            case 1: jj_3_2(); break;
-            case 2: jj_3_3(); break;
-            case 3: jj_3_4(); break;
-          }
-        }
-        p = p.next;
-      } while (p != null);
-      } catch(LookaheadSuccess ls) { }
-    }
-    jj_rescan = false;
-  }
-
-  static private void jj_save(int index, int xla) {
-    JJCalls p = jj_2_rtns[index];
-    while (p.gen > jj_gen) {
-      if (p.next == null) { p = p.next = new JJCalls(); break; }
-      p = p.next;
-    }
-    p.gen = jj_gen + xla - jj_la; p.first = token; p.arg = xla;
-  }
-
-  static final class JJCalls {
-    int gen;
-    Token first;
-    int arg;
-    JJCalls next;
   }
 
 }
